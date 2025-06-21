6b5f85847b9a1ae79da4ff10d2aa64377d35f93f
OmerFaruk69 <omer.kiziltoprak@edu.ece.fr>
2025-06-22 00:25:52 +0200

    update role

--- react-portfolio/src/components/Hero.jsx
+++ react-portfolio/src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
                         <motion.span variants={container(0.5)}
                             initial="hidden"
                             animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-                            Data Analyst
+                            Data Engineer
                         </motion.span>
                         <motion.p variants={container(1)}
                             initial="hidden"
